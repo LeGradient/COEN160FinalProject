a04dd07554cb38e637123c9382dc87db8a33d35e
tehh4ck3r <kmahajani@scu.edu>
2017-03-08 13:42:21 -0800

    Working on RecycleableItem class

---
+++ src/edu/scu/coen160/finalproject/system/TransactionRecord.java
@@ -0,0 +1,23 @@
+package edu.scu.coen160.finalproject.system;
+
+import java.util.Date;
+
+// not sure if this should be public or package protected
+class TransactionRecord {
+    private double price;
+    private RecyclableItem item;
+    private Date timestamp;
+
+    private boolean emptyTransaction = false;
+
+    public TransactionRecord(Date date, RecyclableItem item, double price) {
+        this.timestamp = date;
+        if (price == -1.0 || item == null)
+            emptyTransaction = true;
+
+        this.price = price;
+        this.item = item;
+    }
+
+
+}

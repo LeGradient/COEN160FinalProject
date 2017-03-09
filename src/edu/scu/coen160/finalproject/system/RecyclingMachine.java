9b012e7db074035724fddd772f3ec4060ef5ed0d
Alex Martin <gradient.am@gmail.com>
2017-03-09 09:06:37 -0800

    RecyclingMachine: implemented fields, accessor methods, a constructor, and empty()

--- src/edu/scu/coen160/finalproject/system/RecyclingMachine.java
+++ src/edu/scu/coen160/finalproject/system/RecyclingMachine.java
@@ -1,4 +1,49 @@
 package edu.scu.coen160.finalproject.system;
 
+import java.util.Date;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RecyclingMachine {
+
+    // keep track of the number of existing recycling machines
+    // used to assign unique IDs to each machine
+    private static int objCount = 0;
+
+    // FIELDS
+
+    private String location;
+    private int id;
+    private double capacity;
+    private double money;
+    private HashMap<String, Double> prices;
+    private ArrayList<TransactionRecord> session;
+
+    // GETTERS & SETTERS
+
+    public String getLocation() { return this.location; }
+    public int getId() { return this.id; }
+    public double getCapacity() { return this.capacity; }
+    public double getMoney() { return this.money; }
+    public void addMoney(double money) { this.money += money; }
+    public double getPrice(String material) { return this.prices.get(material); }
+
+    public RecyclingMachine(HashMap<String, Double> prices, double money, double capacity) {
+        // assign ID and increment object counter
+        this.id = objCount++;
+
+        // initialize fields from constructor parameters
+        this.money = money;
+        this.capacity = capacity;
+        this.prices = prices;
+
+        this.session = new ArrayList<>();
+    }
+
+    public void empty() {
+        this.capacity = 0;
+        TransactionRecord record = new TransactionRecord(Date.from(Instant.now()), null, 0);
+        // TODO: write the record to the database
+    }
 }

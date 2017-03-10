8246673441977f29221bcbf09a292e344db60fc9
Alex Martin <gradient.am@gmail.com>
2017-03-09 16:01:44 -0800

    Partially implemented additional methods

--- src/edu/scu/coen160/finalproject/system/RecyclingMachine.java
+++ src/edu/scu/coen160/finalproject/system/RecyclingMachine.java
@@ -11,6 +11,7 @@ public class RecyclingMachine {
     // used to assign unique IDs to each machine
     private static int objCount = 0;
 
+
     // FIELDS
 
     private String location;
@@ -18,7 +19,9 @@ public class RecyclingMachine {
     private double capacity;
     private double money;
     private HashMap<String, Double> prices;
+    private boolean isSession;
     private ArrayList<TransactionRecord> session;
+
 
     // GETTERS & SETTERS
 
@@ -28,6 +31,9 @@ public class RecyclingMachine {
     public double getMoney() { return this.money; }
     public void addMoney(double money) { this.money += money; }
     public double getPrice(String material) { return this.prices.get(material); }
+
+
+    // CONSTRUCTOR
 
     public RecyclingMachine(HashMap<String, Double> prices, double money, double capacity) {
         // assign ID and increment object counter
@@ -41,9 +47,40 @@ public class RecyclingMachine {
         this.session = new ArrayList<>();
     }
 
+
+    // OTHER METHODS
+
+    public void recycleItem(RecyclableItem item) {
+        double price = getPrice(item.getMaterial());
+        TransactionRecord record = new TransactionRecord(Date.from(Instant.now()), item, price);
+        if (this.isSession) {
+            this.session.add(record);
+        } else {
+            // TODO: write the record to the database
+        }
+    }
+
+    public void startSession() {
+        this.isSession = true;
+    }
+
+    public void cancelSession() {
+        this.session = new ArrayList<>();
+        this.isSession = false;
+    }
+
+    public void submitSession() {
+        assert this.isSession = true;
+        for (TransactionRecord record : session) {
+            // TODO: write the record to the database
+        }
+        this.isSession = false;
+    }
+
     public void empty() {
         this.capacity = 0;
         TransactionRecord record = new TransactionRecord(Date.from(Instant.now()), null, 0);
         // TODO: write the record to the database
     }
+
 }

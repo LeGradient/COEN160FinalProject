106832f49aab936af353312de9e5e6fddc04f4fa
tehh4ck3r <kmahajani200@gmail.com>
2017-03-12 22:27:01 -0700

    Started work on the UI

--- src/edu/scu/coen160/finalproject/gui/RecyclingUI.java
+++ src/edu/scu/coen160/finalproject/gui/RecyclingUI.java
@@ -1,6 +1,34 @@
 package edu.scu.coen160.finalproject.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+class RCMPanel extends JPanel {
+    public RCMPanel() {
+        // TODO initialize the GUI for an RCM
+    }
+}
+
+class RMOSPanel extends JPanel {
+    public RMOSPanel() {
+        // TODO initialize the GUI for an RMOS
+    }
+}
 public class RecyclingUI extends JFrame {
+    private JTabbedPane tabs = new JTabbedPane();
+    public RecyclingUI() {
+        super("Recycling Simulation");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int height = screenSize.height;
+        int width = screenSize.width;
+        setSize(width/4 , height/4);
+        setLocationRelativeTo(null);
+
+        tabs.addTab("RCM 1", new RCMPanel());
+        tabs.addTab("RCM 2", new RCMPanel());
+        tabs.addTab("RMOS", new RMOSPanel());
+    }
 }
